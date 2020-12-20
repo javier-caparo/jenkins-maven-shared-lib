f0e554b22fa259ea5b64acc27756223700717cb9
JavierCaparo <55814804+jfcb853@users.noreply.github.com>
2020-12-20 18:24:12 -0500

    Update asfMavenTlpStdBuild.groovy

--- vars/asfMavenTlpStdBuild.groovy
+++ vars/asfMavenTlpStdBuild.groovy
@@ -1,21 +1,32 @@
 def call(){
-  pipeline { 
-    agent any  
-    stages { 
-        stage('Build with java 9') { 
-            steps {
-				withMaven(jdk:'java-9') {
-					sh 'mvn clean verify' 
+	pipeline { 
+		agent any
+		tools {
+			maven 'Maven3'
+		}
+		stages { 
+			stage ('Initialize') {
+				steps {
+					sh '''
+						echo "PATH = ${PATH}"
+						echo "M2_HOME = ${M2_HOME}"
+					'''
 				}
 			}
-		}
-		stage('Build with java 8') { 
-            steps {
-				withMaven(jdk:'java-8') {
-					sh 'mvn clean verify' 
+			stage('Build with java 9') { 
+				steps {
+					withMaven(jdk:'java-9') {
+						sh 'mvn clean verify' 
+					}
+				}
+			}
+			stage('Build with java 8') { 
+				steps {
+					withMaven(jdk:'java-8') {
+						sh 'mvn clean verify' 
+					}
 				}
 			}
 		}
 	}
- }
 }
