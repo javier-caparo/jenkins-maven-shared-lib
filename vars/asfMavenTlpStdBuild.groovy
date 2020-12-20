3b40fbf8b5796aa5f4dd0ff7404432485cf778b4
JavierCaparo <55814804+jfcb853@users.noreply.github.com>
2020-12-20 14:13:37 -0500

    Create asfMavenTlpStdBuild.groovy

---
+++ vars/asfMavenTlpStdBuild.groovy
@@ -0,0 +1,21 @@
+def call(){
+  pipeline { 
+    agent any  
+    stages { 
+        stage('Build with java 9') { 
+            steps {
+				withMaven(jdk:'java-9') {
+					sh 'mvn clean verify' 
+				}
+			}
+		}
+		stage('Build with java 8') { 
+            steps {
+				withMaven(jdk:'java-8') {
+					sh 'mvn clean verify' 
+				}
+			}
+		}
+	}
+ }
+}
